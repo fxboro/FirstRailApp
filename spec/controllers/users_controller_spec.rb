17bf27b8a1f49b27e74375fff26fd0503b30a3ef
fxboro <chimadayo@yahoo.com>
2018-03-14 14:30:27 +0100

    fixes on spec controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,21 +3,19 @@ require 'rails_helper'
 describe UsersController, type: :controller do
 
   before do
-    #@user1 = FactoryGirl.create(:user)
-    @user = User.create!(first_name: "Jonny", last_name: "Gallen", email: "jong@gmail.com", password: "567891", admin: true)
-  end
+    let(:user){ FactoryGirl.create(:user) }
+    let(:user2){ FactoryGirl.create(:user) }
 
-  describe "GET #show" do
+    describe "GET #show" do
      context "when user is logged in" do
        before do
          sign_in @user
        end
 
        it "loads the correct user details" do
-         #get :show, id: @user.id
-         get :show, params: { id: @user1.id }
-         expect(response.status).to eq 200
-         expect(assigns(:user)).to eq @user
+          get :show, params: { id: @user1.id }
+          expect(response.status).to eq 200
+          expect(assigns(:user)).to eq @user
        end
 
        it "doesn't load the second user" do
