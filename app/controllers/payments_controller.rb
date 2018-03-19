4e416800f90dce569202b9dcc5a7cc9e66946493
fxboro <chimadayo@yahoo.com>
2018-03-19 21:07:37 +0100

    modified payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,6 +16,7 @@ class PaymentsController < ApplicationController
 
 		if charge.paid
 			  Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
+        UserMailer.order_placed(@user,@product).deliver_now
 		end
 
 		    flash[:success] = "Payment processed successfully"
